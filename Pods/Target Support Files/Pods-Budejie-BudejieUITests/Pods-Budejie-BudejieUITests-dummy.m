b04550309241e1151ff75baaa87dcb98989a2e35
muziqi <muziqi@zongheng.com>
2020-03-31 18:36:39 +0800

    pods

--- Pods/Target Support Files/Pods-Budejie-BudejieUITests/Pods-Budejie-BudejieUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Budejie_BudejieUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Budejie_BudejieUITests
-@end
--- Pods/Target Support Files/Pods-BudejieTests/Pods-BudejieTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BudejieTests : NSObject
-@end
-@implementation PodsDummy_Pods_BudejieTests
-@end
